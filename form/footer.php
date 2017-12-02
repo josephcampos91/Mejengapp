d53b141da9c51e6fcfe0cffe71dc7964fd99a388
Joseph Campos <josephcampos91@gmail.com>
2017-12-02 14:48:18 -0600

    Se cambian los liks para heroku

--- form/footer.php
+++ form/footer.php
@@ -4,7 +4,7 @@
 		</div>
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-		<script src="http://localhost:8081/mejengapp/tether/dist/js/tether.min.js"></script>
-		<script src="http://localhost:8081/mejengapp/bootstrap/js/bootstrap.min.js"></script>
+		<script src="https://mejeng-app.herokuapp.com/mejengapp/tether/dist/js/tether.min.js"></script>
+		<script src="https://mejeng-app.herokuapp.com/mejengapp/bootstrap/js/bootstrap.min.js"></script>
 	</body>
 </html>
--- form/header.php
+++ form/header.php
@@ -3,8 +3,8 @@
 <head>
     <title></title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" charset="utf-8" />
-    <link rel="stylesheet" type="text/css" href="http://localhost:8081/mejengapp/tether/dist/css/tether.min.css" />
-    <link rel="stylesheet" type="text/css" href="http://localhost:8081/mejengapp/bootstrap/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="https://mejeng-app.herokuapp.com/mejengapp/tether/dist/css/tether.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://mejeng-app.herokuapp.com/mejengapp/bootstrap/css/bootstrap.css" />
 </head>
 <body>
     <div class="container">
